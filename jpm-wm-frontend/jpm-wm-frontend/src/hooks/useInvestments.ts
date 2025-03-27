c892d295c44d55533da454d7f72d2081ce157af2
girish.balekundri <girish.thetechieguy@gmail.com>
2025-03-27 23:47:22 +0800

    Removed all the old design.
    After changing to new design.

--- jpm-wm-frontend/jpm-wm-frontend/src/hooks/useInvestments.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchInvestments } from '../services/api';
-
-export const useInvestments = () => {
-  return useQuery(['investments'], fetchInvestments);
-};
