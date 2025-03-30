2e03b04e897400b6fe9420afadaf5005805c1fec
girish.balekundri <girish.thetechieguy@gmail.com>
2025-03-30 23:08:54 +0800

    Added the Keycloak congigration into gateway and authorize the api.

---
+++ jpm-wm-backend/spc-api-gateway/src/test/java/com/jpmorgan/wm/spc_api_gateway/SpcApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.wm.spc_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpcApiGatewayApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
