09c32d15654af2939085c76824b5ad5c689bfe4a
girish.balekundri <girish.thetechieguy@gmail.com>
2025-04-16 23:40:38 +0800

    updating hard code values into vault and clean the code.

--- jpm-wm-backend/jpm-user-service/src/main/java/com/wm/jpmorgan/jpm_user_service/JpmUserServiceApplication.java
+++ jpm-wm-backend/jpm-user-service/src/main/java/com/wm/jpmorgan/jpm_user_service/JpmUserServiceApplication.java
@@ -1,8 +1,11 @@
 package com.wm.jpmorgan.jpm_user_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
